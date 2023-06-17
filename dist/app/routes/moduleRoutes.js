3968536ea0a0c6db327cb583b49b9838a1f151b7
rabbanidev <golamrabbani227417@gmail.com>
2023-06-17 22:12:30 +0600

    dist

---
+++ dist/app/routes/moduleRoutes.js
@@ -0,0 +1,25 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const auth_route_1 = require("../modules/auth/auth.route");
+const cow_route_1 = require("../modules/cow/cow.route");
+const order_route_1 = require("../modules/order/order.route");
+const user_route_1 = require("../modules/user/user.route");
+const modulesRoutes = [
+    {
+        path: '/auth',
+        route: auth_route_1.AuthRoutes,
+    },
+    {
+        path: '/users',
+        route: user_route_1.UserRoutes,
+    },
+    {
+        path: '/cows',
+        route: cow_route_1.CowRoutes,
+    },
+    {
+        path: '/orders',
+        route: order_route_1.OrderRoutes,
+    },
+];
+exports.default = modulesRoutes;
